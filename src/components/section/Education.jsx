c6c4bed05b778e20ae935174323de034f845f25d
i-manojyadav <i.manojyadav325@gmail.com>
2026-01-31 14:37:28 +0530

    Fix minor UI bugs

--- src/components/section/Education.jsx
+++ src/components/section/Education.jsx
@@ -1,12 +1,14 @@
 import './Education.css'
+import AKTULogo from '../../assets/AKTU.png'
+import UoALogo from '../../assets/UoA.png'
 import EducationItem from '../UI/EducationItem';
 
 function Education() {
     return (
         <>
         <h2>Education</h2>
-        <EducationItem logo="src\assets\AKTU.png" university="Dr. A. P. J. Abdul Kalam Technical University" degree="Master of Computer Applications (MCA)"/>
-        <EducationItem logo="src\assets\UoA.png" university="University of Allahabad" degree="Bachelor of Computer Applications (BCA)"/>
+        <EducationItem logo={AKTULogo} university="Dr. A. P. J. Abdul Kalam Technical University" degree="Master of Computer Applications (MCA)"/>
+        <EducationItem logo={UoALogo} university="University of Allahabad" degree="Bachelor of Computer Applications (BCA)"/>
         </>
     )
 }
