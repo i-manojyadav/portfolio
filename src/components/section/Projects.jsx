b658ec0b372569a30ba4a797f1cce4f9d2ee3745
i-manojyadav <i.manojyadav325@gmail.com>
2026-06-30 17:12:43 +0530

    add: LuxeNest project

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -2,6 +2,7 @@ import './Projects.css'
 import { useState } from 'react'
 import ProjectCard from '../UI/ProjectCard';
 import AssetMitraIMG from '../../assets/AssetMitraIMG.png'
+import LuxeNestIMG from '../../assets/LuxeNestIMG.png'
 
 function Projects() {
 
@@ -17,8 +18,8 @@ function Projects() {
             <div className='project-list'>
                 <button className={activeTab === "AssetMitra" ? "activeTab" : "tab"} onClick={() => setActiveTab("AssetMitra")}>AssetMitra</button>
                 {activeTab === "AssetMitra" && <div>{<ProjectCard img={AssetMitraIMG} title={"AssetMitra"} desc={"It offers a free all-in-one virtual portfolio to track stocks, FDs, and crypto, along with a built-in trading journal to manage and analyze investments and trades efficiently."} techUsed={["React.js", "JavaScript"]} gitLink={"https://github.com/i-manojyadav/assetmitra"} demoLink={"https://assetmitra.vercel.app/"} />}</div>}
-                <button className={activeTab === "Project2" ? "activeTab" : "tab"} onClick={() => setActiveTab("Project2")}>Project 2</button>
-                {activeTab === "Project2" && <p>Soon...</p>}
+                <button className={activeTab === "LuxeNest" ? "activeTab" : "tab"} onClick={() => setActiveTab("LuxeNest")}>Project 2</button>
+                {activeTab === "LuxeNest" && <div>{<ProjectCard img={LuxeNestIMG} title={"LuxeNest"} desc={"LuxeNest is a full-stack web application that allows users to create, manage, and explore property listings. Users can sign up, sign in, add new listings, and leave reviews with rating."} techUsed={["Node.js", "Express.js", "MongoDB", "Mongoose", "EJS", "Bootstrap", "JavaScript"]} gitLink={"https://github.com/i-manojyadav/luxenest"} demoLink={"https://luxenest-jv5n.onrender.com/"} />}</div>}
                 <button className={activeTab === "Project3" ? "activeTab" : "tab"} onClick={() => setActiveTab("Project3")}>Project 3</button>
                 {activeTab === "Project3" && <p>Soon...</p>}
             </div>
